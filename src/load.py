55a9155711feb13678f80c5348bbcdf4109232dd
Curtis Hammons <curtis@cjhammons.com>
2023-10-12 19:31:50 -0700

    implemented load

--- src/load.py
+++ src/load.py
@@ -4,12 +4,22 @@
 # Created: 2023-10-12
 #------------------------------------
 
-# load.py
+import boto3
+import pandas as pd
 
-def load_to_csv(data, filename):
-    # Save data to CSV
-    pass
+def load_to_csv(df, filename):
+    file_path = 'data/' + filename
+    df.to_csv(file_path, index=False)
+    return file_path
     
-def upload_to_s3(file_path, s3_path):
-    # Upload CSV to S3
-    pass
+def upload_to_s3(bucket_name, file_path, object_name=None, s3_client=None):
+    s3 = s3_client
+    if s3 is None:
+        s3 = boto3.client('s3')
+    if object_name is None:
+        object_name = file_path
+    try:
+        s3.upload_file(file_path, bucket_name, object_name)
+        print(f"Successfully uploaded {file_path} to {bucket_name}.")
+    except Exception as e:
+        print(f"Could not upload to S3: {e}")
--- src/main.py
+++ src/main.py
@@ -30,10 +30,24 @@ def main():
     
     # Transform data
     # We pass the extracted data to the transform functions
-    df_incremental_data = calculate_incremental(df_county_population)
+    df_incremental_data = calculate_incremental(df_us_state_cumulative)
     df_rolling_data = calculate_rolling_avg(df_incremental_data)
     df_mask_score = calculate_mask_wearer_score(df_mask_use_by_county, df_county_population)
     
     # Load to CSV and S3
 
-    
+    s3_client = boto3.client(
+        's3',
+        aws_access_key_id=configparser["AWS"]["aws_access_key_id"],
+        aws_secret_access_key=configparser["AWS"]["aws_secret_access_key"],
+        region_name=configparser["AWS"]["region_name"]
+    )
+    bucket_name = configparser["S3"]["bucket_name"]
+
+    incremental_path = load_to_csv(df_incremental_data, 'incremental_data.csv')
+    rolling_path = load_to_csv(df_rolling_data, 'rolling_data.csv')
+    mask_score_path = load_to_csv(df_mask_score, 'mask_score.csv')
+
+    upload_to_s3(bucket_name, incremental_path, s3_client=s3_client)
+    upload_to_s3(bucket_name, rolling_path, s3_client=s3_client)
+    upload_to_s3(bucket_name, mask_score_path, s3_client=s3_client)
