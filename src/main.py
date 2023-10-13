31d002bc37ffa34647422af39e93162825a4cc2f
Curtis Hammons <curtis@cjhammons.com>
2023-10-12 18:31:20 -0700

    implemented extraction logic

--- src/main.py
+++ src/main.py
@@ -3,15 +3,36 @@
 # Email: curtishammons@gmail.com
 # Created: 2023-10-12
 #------------------------------------
-from extract import extract_data
+from extract import *
 from transform import *
 from load import *
+import configparser
+import psycopg2
+import pandas as pd
 
 def main():
-    data = extract_data()
-    incremental_data = calculate_incremental(data)
-    rolling_data = calculate_rolling_average(data)
-    mask_score = calculate_mask_wearer_score(data)
+
+    # Extract data from DB
+    # First we read the DB config from the config.ini file
+    configparser = configparser.ConfigParser()
+    configparser.read("config.ini")
+    conn = psycopg2.connect(
+        host=configparser["DB"]["host"],
+        port=configparser,
+        database=configparser["DB"]["database"],
+        user=configparser["DB"]["user"],
+        password=configparser["DB"]["password"]
+    )
+    df_county_population = extract_county_population(conn)
+    df_mask_use_by_county = extract_mask_use_by_county(conn)
+    df_us_state_cumulative = extract_us_state_cumulative(conn)
+    conn.close()
+    
+    # Transform data
+    # We pass the extracted data to the transform functions
+    # incremental_data = calculate_incremental(data)
+    # rolling_data = calculate_rolling_average(data)
+    # mask_score = calculate_mask_wearer_score(data)
     
     # Load to CSV and S3
     load_to_csv(incremental_data, "incremental.csv")
