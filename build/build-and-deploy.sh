fcf4b4bec8fa1e3c39580e304e97f63ac8c5dad1
Curtis Hammons <curtis@cjhammons.com>
2023-10-16 15:28:39 -0700

    added fatal_if exit conditions

--- build/build-and-deploy.sh
+++ build/build-and-deploy.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+function fatal_if {
+    if [ $1 -ne 0 ]; then
+        echo "Error: $1"
+        exit 1
+    fi
+}
 
 # This script builds the Docker image, pushes it to ECR, and deploys it to EKS as a cronjob.
 
@@ -11,22 +17,31 @@ IMAGE_NAME="covid-19-etl-registry"
 KUBE_DEPLOYMENT_NAME="covid-19-etl-cron"
 
 # Build Docker image
-docker build -t $IMAGE_NAME .
+task="Building Docker image" && echo $task
+docker build --platform linux/amd64 -t $IMAGE_NAME .
+fatal_if $task
 
 # Authenticate Docker with the ECR
+task="Authenticating Docker with ECR" && echo $task
 aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 587344276961.dkr.ecr.us-west-1.amazonaws.com
+fatal_if $task
 
 # Tag the image
+task="Tagging Docker image" && echo $task
 docker tag covid-19-etl-registry:latest 587344276961.dkr.ecr.us-west-1.amazonaws.com/covid-19-etl-registry:latest
+fatal_if $task
 
 # Push to ECR
+task="Pushing Docker image to ECR" && echo $task
 docker push 587344276961.dkr.ecr.us-west-1.amazonaws.com/covid-19-etl-registry:latest
+fatal_if $task
 
 # Update the EKS cluster credentials
 aws eks --region $AWS_REGION update-kubeconfig --name $EKS_CLUSTER
 
 # Deploy to EKS
-# kubectl set image deployment/$KUBE_DEPLOYMENT_NAME $KUBE_DEPLOYMENT_CONTAINER_NAME=$ECR_REGISTRY/$IMAGE_NAME:latest
+task="Deploying to EKS" && echo $task
 kubectl apply -f k8s/cronjob.yaml
+fatal_if $task
 
 echo "Deployment complete."
